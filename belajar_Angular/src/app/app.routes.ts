0e6e954443f146ef61aac331e281846c305d70ab
MikroXD <ugraagung30@gmail.com>
2024-11-20 19:48:18 +0700

    commit 2

--- belajar_Angular/src/app/app.routes.ts
+++ belajar_Angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-import (homeComponent)from ',/home/home.component';
-export const routes: Routes = [{path:'home',component:HomeComponent}];
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+export const routes: Routes = [
+    { 
+        path: '', 
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Details Page'
+    }
+];
