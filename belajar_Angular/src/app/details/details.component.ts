7029e1cc6e2297964c3b572db465a6d807d8c2fe
MikroXD <ugraagung30@gmail.com>
2024-11-20 20:20:46 +0700

    commit 3

--- belajar_Angular/src/app/details/details.component.ts
+++ belajar_Angular/src/app/details/details.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormGroup,FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-     <article >
+     <article>
       <img class="listing-photo" [src]="housingLocation?.photo"
         alt="Exterior photo of {{housingLocation?.name}}"/>
       <section class="listing-description">
@@ -24,6 +25,17 @@ import { HousingLocation } from '../housing-location';
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
+      <section class="listing-apply">
+        <h1 class ="section-heading">Apply to live here</h1>
+        <form (submit)="submitApplyForm()">
+        <label for="first-name">First Name</label>
+         <input type="text" id="first-name" placeholder="Input first name">
+          <label for="last-name">Last Name</label>
+          <input type="text" id="last-name" placeholder="Input last name">
+          <button type="submit" class="primary"> Apply </button>
+        </form>
+      </section>
+    </article>
   `,
   styleUrl: './details.component.css'
 })
@@ -32,11 +44,18 @@ export class DetailsComponent {
   housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined
-  
+  applyForm: FormGroup = new FormGroup({
+    firstName : new FormControl(''),
+    lastName: new FormControl('')
+  })
 
   constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
     console.table(this.housingLocation)
+  }
+
+  submitApplyForm(){
+    alert("Hallo you sumbit a form");
   }
 }
